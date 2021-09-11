9bbfb4b149163636697ff8f959210c688fd973ab
admin <root@localhost>
2021-09-11 02:30:12 -0700

    Refactor NvChad installation

---
+++ .local/bin/installNvChad.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/NvChad/NvChad ~/.config/nvim
+rm ~/.config/nvim/lua/chadrc.lua
+ln -s ~/.config/nvchad/chadrc.lua ~/.config/nvim/lua/chadrc.lua
+nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
