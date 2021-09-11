5221a80b3c37cb55e8ad0d4710f0f34c565b402c
admin <root@localhost>
2021-09-11 16:20:40 -0700

    Modify NvChad install script

--- .local/bin/installNvChad.sh
+++ .local/bin/installNvChad.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 rm -rf ~/.config/nvim
-git clone https://github.com/NvChad/NvChad ~/.config/nvim
+git clone https://github.com/maximusretard/NvChad ~/.config/nvim
+cd ~/.config/nvim
+git checkout retardmods
 rm ~/.config/nvim/lua/chadrc.lua
 ln -s ~/.config/nvchad/chadrc.lua ~/.config/nvim/lua/chadrc.lua
 nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
