e7dce61abee90166dff4eec009e3207cbfbe74ab
admin <root@localhost>
2021-09-11 04:05:22 -0700

    Add vscode bak

---
+++ .config/vscode-bak/install-extensions.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension mads-hartmann.bash-ide-vscode
+code --install-extension matklad.rust-analyzer
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
