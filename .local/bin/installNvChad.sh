c4ef6be644034c6d524142783287403883c221e6
admin <root@localhost>
2022-07-11 05:25:19 -0700

    Add uninstallers

--- .local/bin/installNvChad.sh
+++ .local/bin/installNvChad.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm -rf ~/.config/nvim
-git clone https://github.com/NvChad/NvChad ~/.config/nvim
-rm ~/.config/nvim/lua/chadrc.lua
-ln -s ~/.config/nvchad/chadrc.lua ~/.config/nvim/lua/chadrc.lua
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+#rm ~/.config/nvim/lua/chadrc.lua
+#ln -s ~/.config/nvchad/chadrc.lua ~/.config/nvim/lua/chadrc.lua
 nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
